92396575c6460f117cf7b57a3fb6f4372fca5b26
Shooman Khatri <112597601+ShoomanKhatri@users.noreply.github.com>
2024-04-04 22:20:58 +0545

    string reverse in c

--- DevC/StringLength.c
+++ DevC/StringLength.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    char str[] = "Hello, World!"; // The string to be reversed
-    int length = 0;
+    char str[20] = "Hello, World!"; // The string to be reversed   This code is tested on online c compiler.
+    int length = 0;   
 
     // Calculate the length of the string
     while (str[length] != '\0') {  //  \n0 is null character.
