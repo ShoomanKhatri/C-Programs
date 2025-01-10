6d3a5b0381fb315a8cd7187e17e7032e07676ba4
Shooman Khatri <112597601+ShoomanKhatri@users.noreply.github.com>
2025-01-10 12:42:26 +0545

    Create PostandPreIncrement.c

---
+++ DevC/PostandPreIncrement.c
@@ -0,0 +1,23 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int a, b,c,d,e;
+    
+    a= 5;
+    b=6;
+    c=7;
+    printf("a = %d \n",a);
+    printf("b = %d \n",b);
+    printf("c = %d \n",c);
+    
+    d = a--;
+    printf("a = %d  \n",a);
+    printf("d = %d \n",d);
+    
+    e = --b;
+    printf("b = %d  \n",b);
+    printf("e = %d \n",e);
+
+    return 0;
+}
